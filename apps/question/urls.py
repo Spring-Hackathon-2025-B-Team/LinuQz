3c3688ee815ffd5c96aa6ed1bdbdfe1e60dbacbd
Oxbs <Oxbs2005@yahoo.co.jp>
2025-05-17 16:20:47 +0900

    feat:questionアプリのルーティング設定

---
+++ apps/question/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = "question"
+
+urlpatterns = [
+    path('', views.index_view, name='index'),
+    path('create', views.create_view, name='create'),
+    path('edit', views.edit_view, name='edit'),
+    path('delete', views.delete_view, name='delete'),
+]
--- apps/question/views.py
+++ apps/question/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
-# Create your views here.
+# 問題一覧画面
+@login_required
+def index_view(request):
+    return render(request, 'question/index.html')
+
+# 問題作成画面
+@login_required
+def create_view(request):
+    return render(request, 'question/create.html')
+
+# 問題編集画面
+@login_required
+def edit_view(request):
+    return render(request, 'question/edit.html')
+
+# 問題削除（画面なし）
+@login_required
+def delete_view(request):
+    return redirect('question:index') 

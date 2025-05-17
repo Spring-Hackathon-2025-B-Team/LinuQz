9dfa2dcdf7da23c43744d0a0e4f973009862f8f8
ishiyamatomoai22 <ishiyamatomoaki22@gmail.com>
2025-05-17 18:43:24 +0900

    動作確認用にhistoriesモデルの追加

--- apps/history/models.py
+++ apps/history/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+import uuid
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Histories(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rank_id = models.IntegerField(verbose_name='ランクID', null=False)
+    play_date = models.DateTimeField(verbose_name='実施日時', auto_now_add=True)
+    score = models.IntegerField(verbose_name='得点', null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)

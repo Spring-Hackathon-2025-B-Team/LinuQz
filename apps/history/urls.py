7fe132405f99fa8d636618e51f52e6074a020e17
ishiyamatomoai22 <ishiyamatomoaki22@gmail.com>
2025-05-22 00:05:20 +0900

    feat:historyアプリのurls.pyにルートを追加

--- apps/history/urls.py
+++ apps/history/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import RankingList
+from .views import RankingList, IncorrectList, RetryView, delete_view
 
 
 app_name = "history"
@@ -7,4 +7,8 @@ app_name = "history"
 
 urlpatterns = [
     path('ranking/', RankingList.as_view(), name="ranking")
+    path('incorrect/', IncorrectList.as_view(), name="incorrect")
+    path('retry/<uuid:pk>', RetryView.as_view(), name="retry")
+    path('retry-answer/<uuid:pk>', RetryView.as_view(), name="retry")
+    path('delete/<uuid:pk>/', delete_view, name='delete'),
 ]
